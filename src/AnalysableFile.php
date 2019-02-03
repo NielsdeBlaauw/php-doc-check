188c4f94858584f276359d773895248c4af362ca
Niels de Blaauw <niels.de.blaauw@gmail.com>
2019-02-03 12:24:48 +0100

    Fixes #3, adds cognitive complexity check to metrics (#24)

--- src/AnalysableFile.php
+++ src/AnalysableFile.php
@@ -6,6 +6,10 @@ class AnalysableFile
     public $file;
     protected $parser;
     protected $arguments;
+    protected $metrics = array(
+        'cognitive'  => '\NdB\PhpDocCheck\Metrics\CognitiveComplexity',
+        'cyclomatic' => '\NdB\PhpDocCheck\Metrics\CyclomaticComplexity'
+    );
     
     public function __construct(\SplFileInfo $file, \PhpParser\Parser $parser, \GetOpt\GetOpt $arguments)
     {
@@ -29,7 +33,11 @@ class AnalysableFile
             return $analysisResult;
         }
         $traverser  = new \PhpParser\NodeTraverser();
-        $metric = new Metrics\CyclomaticComplexity();
+        $metricSlug = 'cognitive';
+        if (array_key_exists($this->arguments->getOption('metric'), $this->metrics)) {
+            $metricSlug = $this->arguments->getOption('metric');
+        }
+        $metric = new $this->metrics[$metricSlug];
         $traverser->addVisitor(new \NdB\PhpDocCheck\NodeVisitor($analysisResult, $this->arguments, $metric));
         $traverser->traverse($statements);
         return $analysisResult;
--- tests/Metrics/CognitiveComplexityTest.php
+++ tests/Metrics/CognitiveComplexityTest.php
@@ -2,22 +2,22 @@
 
 namespace NdB\PhpDocCheck\Metrics;
 
-final class CyclomaticComplexityTest extends \PHPUnit\Framework\TestCase
+final class CognitiveComplexityTest extends \PHPUnit\Framework\TestCase
 {
-    public function testMetricWithoutComplexityIsOne()
+    public function testMetricWithoutComplexityIsZero()
     {
         $node = $this->createMock(\PhpParser\Node\Expr\Assign::class);
         $node->method('getType')->willReturn('Expr_Assign');
-        $metric = new CyclomaticComplexity();
-        $this->assertEquals(1, $metric->getValue($node));
+        $metric = new CognitiveComplexity();
+        $this->assertEquals(0, $metric->getValue($node));
     }
 
     public function testCanCalculateSimpleMetric()
     {
         $node = $this->createMock(\PhpParser\Node\Stmt\If_::class);
         $node->method('getType')->willReturn('Stmt_If');
-        $metric = new CyclomaticComplexity();
-        $this->assertEquals(2, $metric->getValue($node));
+        $metric = new CognitiveComplexity();
+        $this->assertEquals(1, $metric->getValue($node));
     }
 
     public function testCanCalculateFunctionWithChildren()
@@ -32,22 +32,31 @@ final class CyclomaticComplexityTest extends \PHPUnit\Framework\TestCase
             $nodeIf1,
             $nodeIf2,
         );
-        $metric = new CyclomaticComplexity();
-        $this->assertEquals(3, $metric->getValue($node));
+        $metric = new CognitiveComplexity();
+        $this->assertEquals(2, $metric->getValue($node));
     }
 
     public function testCanCalculateCaseComplexity()
     {
         $node = $this->createMock(\PhpParser\Node\Stmt\Case_::class);
         $node->cond = true;
-        $metric = new CyclomaticComplexity();
-        $this->assertEquals(2, $metric->getValue($node));
+        $metric = new CognitiveComplexity();
+        $this->assertEquals(0, $metric->getValue($node));
     }
 
-    public function testCanCalculateSpaceshipComplexity()
+    public function testCanCalculateDepthComplexity()
     {
-        $node = $this->createMock(\PhpParser\Node\Expr\BinaryOp\Spaceship::class);
-        $metric = new CyclomaticComplexity();
+        $nodeIf1 = $this->createMock(\PhpParser\Node\Stmt\If_::class);
+        $nodeIf1->method('getType')->willReturn('Stmt_If');
+        $nodeIf2 = $this->createMock(\PhpParser\Node\Stmt\If_::class);
+        $nodeIf2->method('getType')->willReturn('Stmt_If');
+        $nodeIf2->stmts = $nodeIf1;
+
+        $node = $this->createMock(\PhpParser\Node\Stmt\Function_::class);
+        $node->stmts = array(
+            $nodeIf2,
+        );
+        $metric = new CognitiveComplexity();
         $this->assertEquals(3, $metric->getValue($node));
     }
 }
